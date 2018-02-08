a47ecb0e87e0e201f24f7b6dbffd670ff1de8fd6
Kerry Chen <kchen36@stuy.edu>
2018-02-08 14:38:26 -0500

    done

--- canvas.js
+++ canvas.js
@@ -1,14 +1,19 @@
 var canvas = document.getElementById("slate");
 var stop = document.getElementById("trigger");
 var ctx = canvas.getContext("2d");
+var grow = document.getElementById('size');
+var bounce = document.getElementById("bounce");
 ctx.fillStyle = "red";
-var shrink = false;
+
+
 var requestID;
+
 var animate = function(){
+    var shrink = false;
     window.cancelAnimationFrame(requestID);
     var x = 10;
     var drawcircle = function(){
-	ctx.clearRect(0,0,500,500);
+	ctx.clearRect(0,0,652,500);
 	ctx.beginPath();
 	ctx.arc(250,250,x,0, 2* Math.PI);
 	ctx.fill();
@@ -29,8 +34,51 @@ var animate = function(){
     };
     drawcircle();
 }
+
+var animate2 = function(){
+    var x = 25;
+    var y = 240;
+    var xincrease = true;
+    var yincrease = false;
+    window.cancelAnimationFrame(requestID);
+    var bounce = function(){
+	ctx.clearRect(0,0,652,500);
+	ctx.beginPath();
+	ctx.arc(x,y,25,0, 2* Math.PI);
+	ctx.fill();
+	if(x  >= 627){
+	    xincrease = false;
+	}
+	if(x <= 25){
+	    xincrease = true;
+	}
+	if(y  >= 475){
+	    yincrease = false;
+	}
+	if(y <= 25){
+	    yincrease = true;
+	}
+	if(xincrease){
+	    x++;
+	}
+	else{
+	    x--;
+	}
+	if(yincrease){
+	    y++;
+	}
+	else{
+	    y--;
+	}
+	requestID = window.requestAnimationFrame(bounce);
+	console.log(requestID);
+    };
+    window.requestAnimationFrame(bounce);
+}
+
 var stopit = function(){
     window.cancelAnimationFrame(requestID);
 }
 stop.addEventListener('click', stopit);
-canvas.addEventListener('click',animate);
+grow.addEventListener('click',animate);
+bounce.addEventListener('click',animate2);
